8df86cbd25a6e9fb5c95130e37b5a963f3b68866
Tathagat <tathagat1@Tathagats-MacBook-Air.local>
2022-06-18 21:17:36 +0530

    Updates

---
+++ precache-manifest.f42f63ea37f07d63e8e37a3788dcd3ab.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2178402179cb05e1237",
+    "url": "/React-P1/static/css/main.50de6aea.chunk.css"
+  },
+  {
+    "revision": "a2178402179cb05e1237",
+    "url": "/React-P1/static/js/main.a2178402.chunk.js"
+  },
+  {
+    "revision": "155c8bd59874a993670d",
+    "url": "/React-P1/static/js/1.155c8bd5.chunk.js"
+  },
+  {
+    "revision": "5bcfb0d2df6381fcdbcd",
+    "url": "/React-P1/static/js/runtime~main.5bcfb0d2.js"
+  },
+  {
+    "revision": "aa0971ff917c28c4d2ff804f3272df86",
+    "url": "/React-P1/static/media/i1.aa0971ff.jpg"
+  },
+  {
+    "revision": "fc2e7d955586831ab4adcc40b3e05370",
+    "url": "/React-P1/static/media/i3.fc2e7d95.jpg"
+  },
+  {
+    "revision": "9ec6cc1e20a93172d0977b2c788d6ffd",
+    "url": "/React-P1/static/media/i2.9ec6cc1e.jpg"
+  },
+  {
+    "revision": "61399d8cbb8942cf0a48b9d51fa81ff0",
+    "url": "/React-P1/static/media/photo1.61399d8c.jpg"
+  },
+  {
+    "revision": "bd566711e345d22b10668ca97fed3cb5",
+    "url": "/React-P1/static/media/photo2.bd566711.jpg"
+  },
+  {
+    "revision": "079f4127a8334ad537c8b3a0bbcfada1",
+    "url": "/React-P1/static/media/img2.079f4127.jpg"
+  },
+  {
+    "revision": "733d56ac924a997b415a5e28866159f7",
+    "url": "/React-P1/static/media/img3.733d56ac.jpg"
+  },
+  {
+    "revision": "8ef8b66eb2553f021882a668333835e9",
+    "url": "/React-P1/index.html"
+  }
+];
